12417e056918c4cdabaa27475a2f86488f5d2e5f
abd-saad <4bd.saad1999@gmail.com>
2025-09-27 23:19:56 +0700

    PS-4 Backend: Implement signup API

--- internal/usecases/user.go
+++ internal/usecases/user.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/EXRF/POS-Backend/internal/entities"
 	"github.com/EXRF/POS-Backend/internal/repository"
-	"golang.org/x/crypto/bcrypt"
+	"github.com/EXRF/POS-Backend/pkg/utils"
 )
 
 // UserUsecase defines the interface for user-related business logic
@@ -27,28 +27,28 @@ func NewUserUsecase(userRepo repository.UserRepository) *UserUsecaseImpl {
 
 // RegisterUser registers a new user with hashed password
 func (uc *UserUsecaseImpl) RegisterUser(req entities.CreateUserRequest) (*entities.User, error) {
-	// Check if user with email already exists
-	existingUser, _ := uc.userRepo.GetUserByEmail(req.Email)
+	// Check if user with username already exists
+	existingUser, _ := uc.userRepo.GetUserByUsername(req.Username)
 	if existingUser != nil {
-		return nil, errors.New("user with this email already exists")
+		return nil, errors.New(utils.ErrUserExists)
 	}
 
-	// Check if user with username already exists
-	existingUser, _ = uc.userRepo.GetUserByUsername(req.Username)
+	// Check if user with email already exists
+	existingUser, _ = uc.userRepo.GetUserByEmail(req.Email)
 	if existingUser != nil {
-		return nil, errors.New("user with this username already exists")
+		return nil, errors.New(utils.ErrEmailExists)
 	}
 
 	// Hash the password
-	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	hashedPassword, err := utils.HashPassword(req.Password)
 	if err != nil {
-		return nil, errors.New("failed to hash password")
+		return nil, errors.New("Failed to hash password")
 	}
 
 	user := &entities.User{
 		Username:    req.Username,
 		Email:       req.Email,
-		Password:    string(hashedPassword),
+		Password:    hashedPassword,
 		FirstName:   &req.FirstName,
 		LastName:    &req.LastName,
 		PhoneNumber: &req.PhoneNumber,
